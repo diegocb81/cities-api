115d9867be2ebec1f3c82d12b74ae75620f57d3e
Diego Barcellos <diegocb81@gmail.com>
2021-05-19 00:33:48 -0300

    Atualizando o projeto e novo git.init

---
+++ src/main/java/com/github/diegocb81/citiesapi/states/StateRepository.java
@@ -0,0 +1,6 @@
+package com.github.diegocb81.citiesapi.states;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
